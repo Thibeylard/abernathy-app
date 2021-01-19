aa23afd9d6a5d4617ef11423df683d3dba19d8ba
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2021-01-19 14:45:26 +0100

    Enable DiscoveryClient for ClientApp + fixed FeignClient ZUUL name reference

--- client/src/main/java/com/mediscreen/client/proxies/PatientMsProxy.java
+++ client/src/main/java/com/mediscreen/client/proxies/PatientMsProxy.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "zuul-server")
+@FeignClient(name = "mediscreen-zuul-server")
 @RibbonClient(name = "mediscreen-patient-ms")
 public interface PatientMsProxy {
 
