31697f33965ed3e632a7453a115c77a8a9911d38
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2021-01-19 14:10:06 +0100

    Refactored docker-compose.yml and PatientMsProxy to establish zuul-server as API gateway

--- client/src/main/java/com/mediscreen/client/proxies/PatientMsProxy.java
+++ client/src/main/java/com/mediscreen/client/proxies/PatientMsProxy.java
@@ -2,21 +2,23 @@ package com.mediscreen.client.proxies;
 
 import com.mediscreen.common.dtos.PatientDTO;
 import com.mediscreen.patientms.models.Patient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "mediscreen-patient-ms")
+@FeignClient(name = "zuul-server")
+@RibbonClient(name = "mediscreen-patient-ms")
 public interface PatientMsProxy {
 
-    @GetMapping("/patient")
+    @GetMapping("/mediscreen-patient-ms/patient")
     @ResponseBody List<Patient> getAllPatients();
 
-    @GetMapping("/patient/{id}")
+    @GetMapping("/mediscreen-patient-ms/patient/{id}")
     @ResponseBody List<Patient> getPatient(@PathVariable("id") String id);
 
-    @PostMapping("/patient")
+    @PostMapping("/mediscreen-patient-ms/patient")
     @ResponseBody ResponseEntity<?> addPatient(@RequestBody PatientDTO patientDTO);
 }
