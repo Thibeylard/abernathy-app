cb70dbc53fd5cd0e3af12d5325506fa7a6808d3c
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2021-01-21 18:06:27 +0100

    Added test profiles for all modules

--- patient-ms/src/test/java/com/mediscreen/patientms/unit/PatientControllerTest.java
+++ patient-ms/src/test/java/com/mediscreen/patientms/unit/PatientControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class PatientControllerTest {
 
     @Autowired
