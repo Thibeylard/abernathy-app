fcb5faf6acc6727a50e05b43ad477e44e087d1a0
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2021-02-10 11:30:41 +0100

    PatHistory entity validation

--- patHistory/src/main/java/com/mediscreen/abernathyapp/patHistory/validators/PatHistoryValidator.java
+++ patHistory/src/main/java/com/mediscreen/abernathyapp/patHistory/validators/PatHistoryValidator.java
@@ -13,6 +13,20 @@ public class PatHistoryValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
+        PatHistory patHistory = (PatHistory) target;
 
+        if (patHistory.getPatientId() == null || patHistory.getPatientId().isBlank()) {
+            errors.rejectValue(
+                    "patientId",
+                    "patientId.empty",
+                    "patientId field is mandatory");
+        }
+
+        if (patHistory.getContent() == null || patHistory.getContent().isBlank()) {
+            errors.rejectValue(
+                    "content",
+                    "content.empty",
+                    "content field is mandatory");
+        }
     }
 }
