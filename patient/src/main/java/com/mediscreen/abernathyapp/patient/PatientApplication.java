efd12aa4ed9a8b3e8523204fc7c91604a55c29ea
Thibeylard <17107365+Thibeylard@users.noreply.github.com>
2021-02-10 11:16:57 +0100

    Added Spring Retry configuration to mySqlDataSource in Patient

--- patient/src/main/java/com/mediscreen/abernathyapp/patient/PatientApplication.java
+++ patient/src/main/java/com/mediscreen/abernathyapp/patient/PatientApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaRepositories
+@EnableRetry
 public class PatientApplication {
 
 	public static void main(String[] args) {
--- patient/src/main/java/com/mediscreen/abernathyapp/patient/config/ApplicationConfig.java
+++ patient/src/main/java/com/mediscreen/abernathyapp/patient/config/ApplicationConfig.java
@@ -4,8 +4,12 @@ import brave.sampler.Sampler;
 import com.mediscreen.abernathyapp.patient.validators.PatientValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ApplicationConfig {
@@ -31,4 +35,25 @@ public class ApplicationConfig {
         return Sampler.ALWAYS_SAMPLE;
     }
 
+    @Bean
+    @Profile("docker")
+    public DataSource mySqlDataSource() {
+        DataSourceBuilder<? extends DataSource> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mysql://mysqldb:3306/abernathy?serverTimezone=Europe/Paris");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("mysqlPass");
+        return new RetryableDataSourceWrapper(dataSourceBuilder.build());
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource h2DataSource() {
+        DataSourceBuilder<? extends DataSource> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSourceBuilder.username("thibaut");
+        dataSourceBuilder.password("h2Pass");
+        return dataSourceBuilder.build();
+    }
 }
